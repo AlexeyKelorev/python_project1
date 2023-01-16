6e5afddb7f0388eeeb6f635ead348754b8208434
AlexeyKelorev <kelorev@mail.ru>
2023-01-16 22:08:04 +0800

    Homework1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,6 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+number = input("Введите целое положительное число n: ")
+result = int(number) + int(number+number) + int(number+number+number)
+print(f"{number} + {number+number} + {number+number+number} = {result}")
