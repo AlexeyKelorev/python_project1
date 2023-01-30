e62cec70e72710ee5cd1f45fb02256e075577e08
AlexeyKelorev <kelorev@mail.ru>
2023-01-31 00:08:04 +0800

    Homework3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,16 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+
+
+def my_func1(*args):
+    print(sum(sorted(list(args), reverse=True)[:2]))
+
+
+def my_func2(*args):
+    print(sum(list(args)) - min(list(args)))
+
+
+if __name__ == '__main__':
+    my_func1(322, 150, 200)
+    my_func2(322, 150, 200)
--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,18 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+
+
+def get_sum(my_sum=0):
+    lst = input("Вводите числа через пробел: ").split(" ")
+    for num in range(len(lst)):
+        if lst[num] != "q":
+            my_sum = my_sum + int(lst[num])
+        else:
+            print(my_sum)
+            exit('Получен символ выхода.')
+    print(my_sum)
+    get_sum(my_sum)
+
+
+get_sum()
